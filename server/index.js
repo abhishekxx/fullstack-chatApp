30166588560eb15355b391d3c95685d73cdd0eac
your_username <abhishek.chand@ausnepit.com.au>
2023-05-13 15:17:51 +0545

    message form feature

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-// import { Configuration, OpenAIApi } from "openai";
-// import openAiRoutes from "./routes/openai.js";
+import { Configuration, OpenAIApi } from "openai";
+import openAiRoutes from "./routes/openai.js";
 // import authRoutes from "./routes/auth.js";
 
 /* CONFIGURATIONS */
